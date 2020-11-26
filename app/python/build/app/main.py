48b4918f38758ccb9f4ecc981203c194bc2d7da6
sschertenleib <schertenleib@legon.ch>
2020-11-26 16:47:09 +0100

    increased data increment to every 30minutes

--- app/python/build/app/main.py
+++ app/python/build/app/main.py
@@ -1,5 +1,6 @@
 # import the library
 import fhnw_ds_weatherstation_client as weather
+import time
 # import os
 
 # DB and CSV config
@@ -22,3 +23,6 @@ weather.connect_db(config)
 
 # import latest data (delta between last data point in DB and current time)
 weather.import_latest_data(config)
+
+# Wait 30 minutes and then restart
+time.sleep(1800)
