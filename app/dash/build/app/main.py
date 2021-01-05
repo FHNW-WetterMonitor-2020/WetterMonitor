c6d31c3e15e4fb1354f28b552f021fbd63774cde
sschertenleib <schertenleib@legon.ch>
2021-01-05 15:36:53 +0100

    added latest changes

--- app/dash/build/app/main.py
+++ app/dash/build/app/main.py
@@ -1,9 +1,8 @@
 import dash
+import dash_bootstrap_components as dbc
 #from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
-
 import plotly.graph_objs as go
 import pandas as pd
 import datetime
@@ -13,11 +12,15 @@ from Vorhersage import get_datapoint, get_last_timestamp, make_pred, make_timest
 
 client = InfluxDBClient('influxdb', 8086, '', '', 'meteorology')
 
+#Zeitzonen festlegen
+old_timezone = pytz.timezone('UTC')
+new_timezone = pytz.timezone('Europe/Zurich')
+
 #Aktuellster timestamp von Tiefenbrunnen
 last_data = get_last_timestamp("tiefenbrunnen")
 timestamp_now = pd.Timestamp(last_data[0])
 
-#Meldung, wenn nicht die aktuellsten Daten angezeigt werden.
+#Meldung, wenn nicht die aktuellsten Daten angezeigt werden (älter als 35min = 2100s).
 diff_now = (datetime.datetime.now(pytz.utc) - timestamp_now).seconds
 if diff_now > 2100:
     inet = "Bitte prüfen Sie die Internetverbindung."
@@ -37,17 +40,15 @@ for item in datapoint:
 winddir = name_winddir(winddir_now)
 
 #historische Daten
-#str(timestamp_now.minute)
-liste = [str(timestamp_now.year-1), "-", str(timestamp_now.month), "-", str(timestamp_now.day), " ", str(timestamp_now.hour), ":", "00", ":00"]
+liste = [str(timestamp_now.year-1), "-", "{0:0=2d}".format(timestamp_now.month), "-", "{0:0=2d}".format(timestamp_now.day), " ", "{0:0=2d}".format(timestamp_now.hour), ":", "{0:0=2d}".format(timestamp_now.minute), ":00"]
 timestamp_hist = "".join(liste)
-print(timestamp_hist)
 datapoint = get_datapoint(timestamp_hist)
 for item in datapoint:
     temperature_hist = item[0]['air_temperature']
     luftdruck_hist = item[0]['barometric_pressure_qfe']
 
 #Graph für die Lufttemperatur definieren
-liste = [str(timestamp_now.year), "-", str(timestamp_now.month), "-", str(timestamp_now.day), " ", str(timestamp_now.hour-5), ":", str(timestamp_now.minute), ":00"]
+liste = [str(timestamp_now.year), "-", "{0:0=2d}".format(timestamp_now.month), "-", "{0:0=2d}".format(timestamp_now.day), " ", "{0:0=2d}".format(timestamp_now.hour-5), ":", "{0:0=2d}".format(timestamp_now.minute), ":00"]
 timestamp_dauer = "".join(liste)
 query = 'SELECT air_temperature FROM "tiefenbrunnen" WHERE time > $timestamp'
 bind_params = {'timestamp': timestamp_dauer}
@@ -57,7 +58,10 @@ temperaturverlauf = []
 for item in res:
     data = item
 for i in range(len(data)):
-    zeitverlauf.append(item[i]['time'])
+    timestamp = pd.Timestamp(item[i]['time'])
+    old = old_timezone.localize(datetime.datetime(timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute, 0))
+    timestamp_loc = old.astimezone(new_timezone)
+    zeitverlauf.append(timestamp_loc)
     temperaturverlauf.append(item[i]['air_temperature'])
 fig = go.Figure(data=[go.Scatter(x=zeitverlauf, y=temperaturverlauf)])
 fig.update_xaxes(title_text="Uhrzeit")
@@ -74,8 +78,16 @@ if luftdruck_now <= 955:
 else:
     niederschlag = "NEIN"
 
+
+# Zeitzone ändern
+old_now = old_timezone.localize(datetime.datetime(timestamp_now.year, timestamp_now.month, timestamp_now.day, timestamp_now.hour, timestamp_now.minute, 0))
+tst_now_loc = old_now.astimezone(new_timezone)
+timestamp_hist = pd.Timestamp(timestamp_hist)
+old_hist = old_timezone.localize(datetime.datetime(timestamp_hist.year, timestamp_hist.month, timestamp_hist.day, timestamp_hist.hour, timestamp_hist.minute, 0))
+tst_hist_loc = old_hist.astimezone(new_timezone)
+
 app = dash.Dash(
-	external_stylesheets=[dbc.themes.BOOTSTRAP]
+        external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
 
 app.layout = html.Div([
@@ -104,7 +116,7 @@ app.layout = html.Div([
     html.Div([
         html.Div([
             html.H1("Aktuelle Wetterdaten"),
-            html.H3(["Aktualisiert am ", timestamp_now.day, ".", timestamp_now.month, ".", timestamp_now.year, " um ", timestamp_now.hour, ":", timestamp_now.minute]),
+            html.H3(["Aktualisiert am ", tst_now_loc.day, ".", tst_now_loc.month, ".", tst_now_loc.year, " um ", tst_now_loc.hour, ":", tst_now_loc.minute]),
             html.H4(inet),
         ]),
 
@@ -140,18 +152,6 @@ app.layout = html.Div([
 
     ], style={'text-align': 'left', 'display': 'inline-block'}),
 
-    #html.Div([
-        #html.H3(children ="Wassertemperatur"),
-        #dcc.Markdown("So kann ich **Fett** oder *kursiv* schreiben."),
-        #html.P(children ="Die Wassertemperatur beträgt:"),
-        #html.P(children = [water_temp, " °C"])
-    #], style={'color': 'blue', 'fontSize': 14}),
-
-    #html.Div([
-        #html.Div([
-            #html.H1(" __ ")
-        #])
-    #], style={'display': 'inline-block'}),
 
     #rechter Abschnitt: Wettervorhersage
     html.Div([
@@ -160,7 +160,7 @@ app.layout = html.Div([
         ]),
 
         html.Div([
-            html.P(["Lufttemperatur um ", (timestamp_now.hour +s)%24, ":", timestamp_now.minute, " Uhr"]),
+            html.P(["Lufttemperatur um ", (tst_now_loc.hour +s)%24, ":", tst_now_loc.minute, " Uhr"]),
             html.H3([temperature_pred, " °C"]),
         ], style={'marginBottom': 30, 'marginTop': 25}),
 
@@ -171,7 +171,7 @@ app.layout = html.Div([
 
         html.Div([
             html.H1("Historische Daten"),
-            html.H3(["von ", timestamp_hist]),
+            html.H3(["vom ", tst_hist_loc.day, ".", tst_hist_loc.month, ".", tst_hist_loc.year, " um ", tst_hist_loc.hour, ":", tst_hist_loc.minute]),
         ]),
 
         html.Div([
@@ -190,4 +190,4 @@ app.layout = html.Div([
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host = '0.0.0.0')
+    app.run_server(debug=True, host='0.0.0.0')
